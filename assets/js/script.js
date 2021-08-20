af6d6acf17514d2f6c8b20195b8a8648730c1089
Technology Community <77574667+tecopro@users.noreply.github.com>
2021-08-20 16:55:15 +0700

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-require(['barangberharga/db/array.js'], function () {
-  require(['barangberharga/dist/bootstrap5/js/bootstrap.bundle.min.js'], function () {
-    require(['barangberharga/js/variable.js'], function () {
-      require(['barangberharga/js/function.js'], function () {
-        require(['barangberharga/js/run.js']);
-      });
-    });
-  });
-});
